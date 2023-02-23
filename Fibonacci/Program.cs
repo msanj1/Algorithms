a4b49a13fdd3e518fb08e695d9a8aba682933dd1
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2023-02-24 07:51:34 +1000

    Implemented Fibonacci algo

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nthNumber = int.Parse(Console.ReadLine());
+
+            var fibList = FibonacciList(nthNumber);
+
+            Console.WriteLine(Fibonacci(nthNumber, fibList));
+        }
+
+        static int Fibonacci(int nthNumber, List<int> fibonacciList)
+        {
+            if (nthNumber <= 1)
+                return nthNumber;
+
+            return fibonacciList[nthNumber];
+        }
+
+        static List<int> FibonacciList(int nthNumber)
+        {
+            var output = new List<int>();
+            output.Add(0);
+            output.Add(1);
+
+            for(int i = 2; i <= nthNumber; i++)
+            {
+                output.Add(output[i - 2] + output[i - 1]);
+            }
+
+            return output;
+        }
+    }
+}

3deb64e405996a878a40bfbedabb8bd2b335c71e
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2023-02-28 08:02:26 +1000

    Fibonacci Partial Sum

---
+++ FibonacciSumOfLastDigit/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FibonacciSumOfLastDigit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nthNumber = long.Parse(Console.ReadLine());
+            var pisanoPeriod = CalculatePisanoPeriod(nthNumber, 10);
+            List<long> fibList = new List<long>();
+
+            if(pisanoPeriod == 0)
+            {
+                fibList = FibonacciList(nthNumber, 10);
+            }
+            else
+            {
+                fibList = FibonacciList(nthNumber % pisanoPeriod, 10);
+            }
+
+            Console.WriteLine(fibList.Sum() % 10);
+        }
+
+        static long Fibonacci(int nthNumber, List<long> fibonacciList)
+        {
+            if (nthNumber <= 1)
+                return nthNumber;
+
+            return fibonacciList[nthNumber];
+        }
+
+        static List<long> FibonacciList(long nthNumber, int mod)
+        {
+            var output = new List<long>();
+            if(nthNumber >= 0)
+                output.Add(0);
+            if (nthNumber >= 1)
+                output.Add(1);
+
+            for (int i = 2; i <= nthNumber; i++)
+            {
+                output.Add((output[i - 2] + output[i - 1]) % mod);
+            }
+
+            return output;
+        }
+
+        static int CalculatePisanoPeriod(long nthNumber, int mod)
+        {
+            int pisanoPeriod = 0;
+
+            var twoBefore = 0;
+            var previous = 1;
+
+            for (int i = 2; i <= nthNumber; i++)
+            {
+                var current = ((twoBefore) + (previous)) % mod;
+                if (current == 1 && previous == 0)
+                {
+                    pisanoPeriod = i - 1;
+                    return pisanoPeriod;
+                }
+
+                twoBefore = previous;
+                previous = current;
+            }
+
+            return pisanoPeriod;
+
+        }
+    }
+}
---
+++ FibonaciPartialSumLastDigit/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FibonaciPartialSumLastDigit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToList();
+            var pisanoPeriod = CalculatePisanoPeriod(input[1], 10);
+            List<long> fibList = new List<long>();
+
+            if (pisanoPeriod == 0)
+            {
+                fibList = FibonacciList(input[0], input[1], 10);
+            }
+            else
+            {
+                var from = input[0] % pisanoPeriod;
+                var to = input[1] % pisanoPeriod;
+
+                if (to < from)
+                {
+                    to = to + pisanoPeriod; //https://www.geeksforgeeks.org/last-digit-of-sum-of-numbers-in-the-given-range-in-the-fibonacci-series/
+                }
+                fibList = FibonacciList(from, to, 10);
+            }
+
+            Console.WriteLine(fibList.Sum() % 10);
+        }
+
+        static List<long> FibonacciList(long from, long to, int mod)
+        {
+            var output = new List<long>();
+            if(from <= 1)
+            {
+                output.Add(0);
+            }
+            if(from <= 1 && to >= 1)
+            {
+                output.Add(1);
+            }
+
+            var twoBefore = 0;
+            var previous = 1;
+
+            for (int i = 2; i <= to; i++)
+            {
+                var current = ((twoBefore) + (previous)) % mod;
+                if(i >= from)
+                {
+                    output.Add(current);
+                }
+
+                twoBefore = previous;
+                previous = current;
+            }
+
+            return output;
+        }
+
+        static int CalculatePisanoPeriod(long nthNumber, int mod)
+        {
+            int pisanoPeriod = 0;
+
+            var twoBefore = 0;
+            var previous = 1;
+
+            for (int i = 2; i <= nthNumber; i++)
+            {
+                var current = ((twoBefore) + (previous)) % mod;
+                if (current == 1 && previous == 0)
+                {
+                    pisanoPeriod = i - 1;
+                    return pisanoPeriod;
+                }
+
+                twoBefore = previous;
+                previous = current;
+            }
+
+            return pisanoPeriod;
+
+        }
+    }
+}

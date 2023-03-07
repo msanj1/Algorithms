1a4751b650486d41d75c4cc09350298b78ed72ad
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2023-03-08 07:42:42 +1000

    Money Change Problem

---
+++ Greedy/MoneyChange/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MoneyChange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var money = int.Parse(Console.ReadLine());
+            var count = 0;
+            while(money > 0)
+            {
+                if(money >= 10)
+                {
+                    money -= 10;
+                    count++;
+                }else if (money >= 5)
+                {
+                    money -= 5;
+                    count++;
+                }
+                else if (money >= 1)
+                {
+                    money -= 1;
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}

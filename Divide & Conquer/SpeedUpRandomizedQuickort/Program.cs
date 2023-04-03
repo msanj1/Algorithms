e4013d720998f10af550272480384f8fea1d3bec
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2023-04-04 08:28:43 +1000

    Implemented quicksort with 3way partition

--- Divide & Conquer/SpeedUpRandomizedQuickort/Program.cs
+++ Divide & Conquer/SpeedUpRandomizedQuickort/Program.cs
@@ -9,27 +9,27 @@ namespace SpeedUpRandomizedQuickort
         static void Main(string[] args)
         {
             var numberOfInput = int.Parse(Console.ReadLine());
-            var input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToList();
+            var input = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToList();
             QuickSort(input);
 
-            Console.WriteLine(string.Join(' ',input));
+            Console.WriteLine(string.Join(" ",input));
         }
 
-        static void QuickSort(List<int> input)
+        static void QuickSort(List<long> input)
         {
             Sort(input, 0, input.Count - 1);
         }
 
-        static void Sort(List<int> input, int left, int right)
+        static void Sort(List<long> input, int left, int right)
         {
             if (left >= right)
             {
                 return;
             }
 
-            var m = PartitionRandomisedPivot(input, left, right);
-            Sort(input, left, m - 1);
-            Sort(input, m + 1, right);
+            var m = Partition3(input, left, right);
+            Sort(input, left, m[0] - 1);
+            Sort(input, m[1] + 1, right);
         }
 
         static int PartitionStandard(List<int> input, int left, int right)
@@ -51,6 +51,42 @@ namespace SpeedUpRandomizedQuickort
             Swap(input, pivot, j);
 
             return j;
+        }
+
+        static List<int> Partition3(List<long> input, int left, int right)
+        {
+            var rand = new Random();
+            var pivot = rand.Next(left, right);
+            Swap(input, pivot, left);
+            pivot = left;
+
+            int j = left;
+            int r = right;
+            int i = left + 1;
+
+            while (i <= r)
+            {
+                var value = input[i];
+                if (value < input[pivot])
+                {
+                    j++;
+                    Swap(input, i, j);
+                    i++;
+                }
+                else if (value > input[pivot])
+                {
+                    Swap(input, i, r);
+                    r--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            Swap(input, pivot, j);
+
+            return new List<int> { j, r };
         }
 
         static int PartitionRandomisedPivot(List<int> input, int left, int right)
@@ -78,6 +114,13 @@ namespace SpeedUpRandomizedQuickort
         }
 
         static void Swap(List<int> input, int from, int to)
+        {
+            var temp = input[from];
+            input[from] = input[to];
+            input[to] = temp;
+        }
+
+        static void Swap(List<long> input, int from, int to)
         {
             var temp = input[from];
             input[from] = input[to];

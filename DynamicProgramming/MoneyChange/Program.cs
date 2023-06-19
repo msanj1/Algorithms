a349d316d99ce2be8b90e7656387de13f8f1951a
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2023-06-20 08:35:58 +1000

    implemented money change algorithm

---
+++ DynamicProgramming/MoneyChange/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoneyChange
+{
+    internal class Program
+    {
+        static Dictionary<int, int> memo = new Dictionary<int, int>();
+        static void Main(string[] args)
+        {
+            var money = int.Parse(Console.ReadLine());
+            Console.WriteLine(MoneyChange(money, new int[] {1, 3, 4}));
+        }
+
+        static int MoneyChange(int money, int[] coins)
+        {
+            int output;
+            if(memo.TryGetValue(money,out output))
+            {
+                return output;
+            }
+
+            if (money == 0)
+            {
+                return 0;
+            }
+
+            var minChange = int.MaxValue;
+            for(int i=0; i< coins.Length; i++)
+            {
+                var coin = coins[i];
+                if (money >= coin)
+                {
+                    var change = MoneyChange(money - coin, coins);
+                    memo[money - coin]=change;
+                    if (change != int.MaxValue && change < minChange)
+                    {
+                        minChange = change + 1;
+                    }
+                }
+            }
+
+            return minChange;
+        }
+    }
+}
---
+++ DynamicProgramming/RecursiveChange/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RecursiveChange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var money = int.Parse(Console.ReadLine()) ;
+            Console.WriteLine(RecursiveChange(money, 9, 6, 5, 1));
+        }
+
+        static int RecursiveChange(int money, params int[] coins)
+        {
+            if (money == 0)
+            {
+                return 0;
+            }
+
+            var minNumberOfCoins = int.MaxValue;
+            for(int i=0; i < coins.Length; i++)
+            {
+                var coin = coins[i];
+                if (money >= coin)
+                {
+                    var numberOfCoins = RecursiveChange(money - coin, coins);
+                    if (numberOfCoins != int.MaxValue && numberOfCoins + 1 < minNumberOfCoins)
+                    {
+                        minNumberOfCoins = numberOfCoins + 1;
+                    }
+                }
+            }
+
+            return minNumberOfCoins;
+        }
+    }
+}

0ec8e00d53efe78536728b9bb84a844bca845c93
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2023-07-19 08:03:27 +1000

    updated the 3 partition algorithm

--- DynamicProgramming/ThreePartition/Program.cs
+++ DynamicProgramming/ThreePartition/Program.cs
@@ -11,6 +11,7 @@ namespace ThreePartition
             var numberOfInputs = int.Parse(Console.ReadLine());
             var inputs = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
             Console.WriteLine(Partition(inputs) ? 1 : 0);
+
         }
 
         static bool Partition(List<int> inputs)
@@ -22,50 +23,45 @@ namespace ThreePartition
             return Partition(inputs, inputs.Count - 1, sum / 3, sum / 3, sum / 3);
         }
 
-        private static Dictionary<string, bool> memo = new Dictionary<string, bool>();
-        public static bool Partition(List<int> inputs, int currentCount, int partitionA, int partitionB, int partitionC)
+        static bool Partition(List<int> inputs, int n, int partitionA, int partitionB, int partitionC)
         {
-            // return true if the subset is found
-            if (memo.ContainsKey($"{partitionA}-{partitionB}-{partitionC}-{currentCount}"))
+            if (memo.ContainsKey($"{partitionA}-{partitionB}-{partitionC}-{n}"))
             {
-                return memo[$"{partitionA}-{partitionB}-{partitionC}-{currentCount}"];
+                return memo[$"{partitionA}-{partitionB}-{partitionC}-{n}"];
             }
-
             if (partitionA == 0 && partitionB == 0 && partitionC == 0)
-            {
                 return true;
-            }
 
-            // base case: no items left
-            if (currentCount < 0)
-            {
+            if (n < 0)
                 return false;
-            }
 
-            // Case 1. The current item becomes part of the first subset
-            var A = false;
-            if (partitionA - inputs[currentCount] >= 0)
+            var currentValue = inputs[n];
+            var statusA = false;
+            var statusB = statusA;
+            var statusC = statusB;
+            if (partitionA - currentValue >= 0)
             {
-                A = Partition(inputs, currentCount - 1, partitionA - inputs[currentCount], partitionB, partitionC);
+                partitionA = partitionA - currentValue;
+                statusA = Partition(inputs, n - 1, partitionA, partitionB, partitionC);
             }
 
-            // Case 2. The current item becomes part of the second subset
-            var B = false;
-            if (!A && (partitionB - inputs[currentCount] >= 0))
+            if (partitionB - currentValue >= 0)
             {
-                B = Partition(inputs, currentCount - 1, partitionA, partitionB - inputs[currentCount], partitionC);
+                partitionB = partitionB - currentValue;
+                statusB = Partition(inputs, n - 1, partitionA, partitionB, partitionC);
             }
 
-            // Case 3. The current item becomes part of the third subset
-            var C = false;
-            if ((!A && !B) && (partitionC - inputs[currentCount] >= 0))
+            if (partitionC - currentValue >= 0)
             {
-                C = Partition(inputs, currentCount - 1, partitionA, partitionB, partitionC - inputs[currentCount]);
+                partitionC = partitionC - currentValue;
+                statusC = Partition(inputs, n - 1, partitionA, partitionB, partitionC);
             }
 
-            // return true if we get a solution
-            memo[$"{partitionA}-{partitionB}-{partitionC}-{currentCount}"] = A || B || C;
-            return A || B || C;
+            memo[$"{partitionA}-{partitionB}-{partitionC}-{n}"] = statusA || statusB || statusC;
+
+            return statusA || statusB || statusC;
         }
+
+        private static Dictionary<string, bool> memo = new Dictionary<string, bool>();
     }
 }
